6a3a8ee345d6a8ace8690945dcef5ed7cd97c8de
studiomfb <57592762+studioMFB@users.noreply.github.com>
2020-08-27 21:26:30 +0100

    Add files via upload

--- scripts/scriptPopUpBoxe.js
+++ scripts/scriptPopUpBoxe.js
@@ -1,13 +1,6 @@
 function openPopup() {
-    document.getElementById('popup1').style.display = 'block';
+    document.getElementById('box').style.display = 'block';
 }
 function closePopup() {
-    document.getElementById('popup1').style.display = 'none';
-}
-
-function openPopup() {
-    document.getElementById('popup2').style.display = 'block';
-}
-function closePopup() {
-    document.getElementById('popup2').style.display = 'none';
+    document.getElementById('box').style.display = 'none';
 }
