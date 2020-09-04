768d11239984a19e08d4c31d804b1eedbdf9d9a5
studiomfb <57592762+studioMFB@users.noreply.github.com>
2020-09-04 15:21:45 +0100

    Add files via upload

---
+++ scripts/lab_scriptButtonCounter.js
@@ -0,0 +1,34 @@
+/*
+var count = 0;
+var butCounterLabel = document.getElementById('butCounter');
+butCounterLabel.textContent = 'Click me, please!';
+var butCount = document.getElementById('butCounter');
+var butCountTxt = document.getElementById('butCounterTxt');
+butCount.onclick = function(){
+    count++;
+    butCountTxt.innerHTML = count;
+}
+*/
+function Counter(name, count){
+    this.name = name;
+    this.count = count;
+    /*
+    this.displayCounter = function() {
+    this.count++;
+    return butCounterTxt.innerHTML = this.count;
+    };
+    */
+}
+
+var counter1 = new Counter('I count how many time you click me!', 0);
+
+var counter1Name = document.getElementById('butCounterName');
+counter1Name.textContent = counter1.name;
+
+var counter1Counter = document.getElementById('butCounter');
+var counter1Txt = document.getElementById('butCounterTxt');
+
+counter1Counter.onclick = function(){
+  counter1.count++;
+  counter1Txt.innerHTML = counter1.count;
+}
