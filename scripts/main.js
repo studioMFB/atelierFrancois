6fc20aeea1beab8000db068990bd90aebe25bbab
studiomfb <57592762+studioMFB@users.noreply.github.com>
2020-11-09 08:49:50 +0000

    uodated SceneGrapgh older working version

--- scripts/main.js
+++ scripts/main.js
@@ -29,43 +29,7 @@ function onLoad() {
 
         return originMatrix;
     }
-
-    // TRY VERSION WITH RENNDER VISITOR
-    function gameLoop() {
-        //var thisTime, deltaTime;
-
-        //thisTime = Date.now();
-        //deltaTime = thisTime - lastTime;
-
-        context.clearRect(-canvas.width/2, -canvas.height/2, canvas.width, canvas.height);
-
-        //document.addEventListener("keydown", keyDownHandler, false);
-        //document.addEventListener("keyup", keyUpHandler, false);
-        //moveUpdate();
-
-        //rootSceneGraphNode.update(); // not yet used
-        rootSceneGraphNode.draw(context, originMatrix);
-
-        //lastTime = thisTime;
-        requestAnimationFrame(gameLoop);
-    }
-    function draw() {
-        originMatrix = setCanvasOrigin();
-
-        renderVisitor = new RenderVisitor();
-        rootSceneGraphNode = new SceneGraphNode();
-
-        background = new Background(rootSceneGraphNode, canvas, new Vector(-canvas.width/2, -canvas.height/2, 1));
-        player = new Player(context, rootSceneGraphNode,new Vector(-canvas.width/2, -canvas.height/2, 1), 1); // why not x=0 y=0
-        
-        rootSceneGraphNode.addChild(background);
-        rootSceneGraphNode.addChild(player);
-
-        gameLoop();
-    }
-
     // WORKING VERSION WITHOUT RENDER VISITOR
-    /*
     function gameLoop() {
         var thisTime, deltaTime;
 
@@ -73,7 +37,6 @@ function onLoad() {
         deltaTime = thisTime - lastTime;
 
         context.clearRect(-canvas.width/2, -canvas.height/2, canvas.width, canvas.height);
-        //refreshBackground(); // NOT there or it will keep on adding to the children array
 
         document.addEventListener("keydown", keyDownHandler, false);
         document.addEventListener("keyup", keyUpHandler, false);
@@ -91,29 +54,27 @@ function onLoad() {
 
         background = new Background(rootSceneGraphNode, canvas, new Vector(-canvas.width/2, -canvas.height/2, 1));
         player = new Player(context, rootSceneGraphNode,new Vector(-canvas.width/2, -canvas.height/2, 1), 1); // why not x=0 y=0
-        //player = new Player(context, rootSceneGraphNode,new Vector(0, 0, 1), 1); // why not x=0 y=0
-        
+
         rootSceneGraphNode.addChild(background);
         rootSceneGraphNode.addChild(player);
 
         gameLoop();
     }
-    */
 
     function moveUpdate() {
-        
+
         if(upPressed) {
             player.moveUp();
         }
         else if(downPressed) {
             player.moveDown();
-        } 
+        }
         else if(leftPressed) {
             player.moveLeft();
         }
         else if(rightPressed) {
             player.moveRight();
-        }        
+        }
     }
     function keyDownHandler(e) {
         if(e.key == "up" || e.key == "ArrowUp") {
@@ -127,7 +88,7 @@ function onLoad() {
         }
         else if(e.key == "left" || e.key == "ArrowLeft") {
             leftPressed = true;
-        }        
+        }
     }
     function keyUpHandler(e) {
         if(e.key == "up" || e.key == "ArrowUp") {
@@ -145,25 +106,7 @@ function onLoad() {
     }
 
     initialiseContext();
-    //draw();
-
-    // TO TEST DRAWING
-    setCanvasOrigin();
-    /*
-    var vectors = [new Vector(0, 0, 1), new Vector(0, -50, 1), new Vector(30, -70, 1), new Vector(30, -40, 1),
-        new Vector(40, -30, 1), new Vector(50, 0, 1), new Vector(10, 30, 1)];
-    //pFillColour, pStrokeColour, pLineThickness, pLineJoin, pVectors, pNumSegments, pRadius, pCenterX, pCenterY
-    var object = new Polygon(null, "#FFFFFF", 1, null, vectors);
-    object.draw(context);
-    */
-    // SHOULD TRY TO RANDOMLY GENERATE THE ASTEROIDS
-    //
-    var vectors = [new Vector(0, 0, 1), new Vector(0, -50, 1), new Vector(50, -70, 1), new Vector(50, -40, 1),
-        new Vector(70, -30, 1), new Vector(50, 0, 1), new Vector(10, 20, 1)];
-    //pFillColour, pStrokeColour, pLineThickness, pLineJoin, pVectors, pNumSegments, pRadius, pCenterX, pCenterY
-    var object = new Polygon(null, "#FFFFFF", 1, null, vectors);
-    object.draw(context);
-    //
+    draw();
 }
 
 window.addEventListener('load', onLoad, false);
--- scripts/nodes/sceneGraphNode.js
+++ scripts/nodes/sceneGraphNode.js
@@ -1,3 +1,4 @@
+/*
 class SceneGraphNode extends RenderVisitor {
     constructor() {
 
@@ -11,24 +12,23 @@ class SceneGraphNode extends RenderVisitor {
         pVisitor.visit(this);
     }
 }
-/*
+*/
 // task 1
 class SceneGraphNode {
     constructor(pLocalMatrix) {
 
     this.mChildren = []; // task 2
-    this.setLocalMatrix(pLocalMatrix); // task 1 
+    this.setLocalMatrix(pLocalMatrix); // task 1
     this.mLastTime = Date.now();
 
     }
-    // task 1
     getLocalMatrix() {
         return this.mLocalMatrix;
     }
     setLocalMatrix(pLocalMatrix) {
         this.mLocalMatrix = pLocalMatrix;
     }
-    // task 2
+
     getNumberOfChildren() {
        return this.mChildren.length;
     }
@@ -46,15 +46,12 @@ class SceneGraphNode {
         this.mPosition = pPosition;
     }
 
-
-    // task 3
     draw(pContext, pWorldTransformMatrix) {
         var i, localTransformMatrix, numberOfChildren, child;
 
         localTransformMatrix = pWorldTransformMatrix.multiply(this.getLocalMatrix());
         localTransformMatrix.setTransform(pContext);
 
-        // task 4
         numberOfChildren = this.getNumberOfChildren();
 
         for (i = 0; i < numberOfChildren; i += 1 ) {
@@ -65,89 +62,8 @@ class SceneGraphNode {
 
         pWorldTransformMatrix.setTransform(pContext);
     }
-    
-    newPosition(pWorldMatrix) {
-        var x, y, translate, newPosition;
-
-        x = this.getPosition().getX();
-        y = this.getPosition().getY();
-   
-        newPosition = new Vector(x, y, 1);
-        this.setPosition(newPosition);
-        translate = Matrix.createTranslation(this.getPosition());
-        this.mLocalTransformMatrix = pWorldMatrix.multiply(translate);
-    }
-
-    moveUp() {
-        var y = this.getPosition().getY();
-        y -= 200;
-        this.getPosition().setY(y);
-    }
-    moveDown() {
-        var y = this.getPosition().getY();
-        y += 50;
-        this.getPosition().setY(y);
-    }
-    moveLeft() {
-        var rotate = this.getRotation();
-        rotate = -Math.PI/4;
-        //rotate -= 10;
-        this.setRotation(rotate);
-    }
-    moveRight() {
-        var rotate = this.getRotation();
-        rotate = Math.PI/3;
-        //rotate += 10;
-        this.setRotation(rotate);
-    }
-
-
 
     update(pDeltaTime) {
-        /*
-        var x, y, velocityX, velocityY, jumpFrame;
-        var thisTime, deltaTime, delay;
-
-        thisTime = Date.now();
-        deltaTime = thisTime - this.mLastTime;
-
-        var delay = 1000;
-   
-        if (pDeltaTime >= delay) {
-            jumpFrame = 1;
-        }
-        else {
-            //jumpFrame = 0;
-        }
-        if ((deltaTime - delay) > delay){
-            this.mLastTime = thisTime;
-            //jumpFrame = 0;
-        } 
-        else {
-        this.mLastTime += delay;
-        jumpFrame = 0;
-        }
-
-        return jumpFrame;
-        */
-
-        //this.mX = (pDeltaTime * Math.floor(Math.random() * 1501));
-        //this.mY = (pDeltaTime * Math.floor(Math.random() * 1501));
-
-        //this.mX += pDeltaTime;
-        //this.mY += pDeltaTime;
-
-
-        //Move with velocity x/y
-        //this.getPosition().getX() += velocityX * pDeltaTime;
-        //this.getPosition().getY() += velocityY * pDeltaTime;
-        /*
-        if(this.mUpdateCounter < this.mUpdateLimit) {
-            this.mUpdateCounter += 1;
-            var newRotation = this.getRotation() + (this.getRotation() * (pDeltaTime/2000));
-            this.setRotation(newRotation);
-        }
-        
     }
+
 }
-*/
