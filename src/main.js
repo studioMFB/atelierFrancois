0f54c977a6049f2a2b617b1269d6363097f9741e
studiomfb <57592762+studioMFB@users.noreply.github.com>
2023-09-29 21:30:19 +0100

    update import

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // import { createApp } from "./../node_modules/vue";
 // import App from "./App.vue";
-import {ModelViewer} from "./components/modelViewer/ModelViewer";
+import ModelViewer from "./components/modelViewer/ModelViewer.ts";
 
 
 async function run() {
