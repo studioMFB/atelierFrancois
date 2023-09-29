f399fef0bb380ee1a17141dfbbc88a4676ebbd33
studiomfb <57592762+studioMFB@users.noreply.github.com>
2023-09-29 22:12:51 +0100

    Add base to config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+const ASSET_URL = process.env.ASSET_URL || '';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `${ASSET_URL}/dist/`,
   plugins: [
     vue(),
     vueJsx(),
