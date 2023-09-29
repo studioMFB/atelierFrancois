6c06ad2fd92ee29f46f94677e07106093e6d1d74
studiomfb <57592762+studioMFB@users.noreply.github.com>
2023-09-29 21:36:56 +0100

    update import

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 // import { createApp } from "./../node_modules/vue";
 // import App from "./App.vue";
-import ModelViewer from "./components/modelViewer/ModelViewer.ts";
+import ModelViewer from "./components/modelViewer/ModelViewer";
 
 
 async function run() {
     console.log("RUN");
     
-    const container = document.getElementById("App");
+    const container = document.getElementById("App") as HTMLElement;
     const modelViewer = new ModelViewer(container);
     
     modelViewer.start();
