764e5642b1e76a141f7b7ee3d10e189f0dcb9da6
Francois Brunet <francois.brunet@visr-vr.com>
2023-09-29 16:33:28 +0100

    Move style import location

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
-import './assets/main.css';
-// import { Vector3 } from "./../node_modules/three";
-// import { Vector3 } from "three";
 import { createApp } from "./../node_modules/vue";
 import App from "./App.vue";
 import {ModelViewer} from "./components/modelViewer/ModelViewer";
-// import router from './router.vue';
-// import { Terrain } from "./components/modelViewer/Terrain";
 
 
 async function run() {
