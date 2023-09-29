2fb18e8be69940fe41511872d94ba0b04309e5ce
Francois Brunet <francois.brunet@visr-vr.com>
2023-09-29 15:02:40 +0100

    change extension of main

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import { Terrain } from "./components/modelViewer/Terrain";
 async function run() {
     console.log("RUN");
     
-    const container = document.getElementById("App") as HTMLElement;
+    const container = document.getElementById("App");
     const modelViewer = new ModelViewer(container);
     
     const terrain01 = new Terrain("T01", new Vector3(2, .5, 2), new Vector3(50, 1, 50), new Vector3(1,0,0));
