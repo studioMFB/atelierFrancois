fa8a73d6322dd3a62e7a2d5b443d3e656723e594
studiomfb <57592762+studioMFB@users.noreply.github.com>
2023-09-29 19:48:54 +0100

    remove menu

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { createApp } from "./../node_modules/vue";
-import App from "./App.vue";
+// import { createApp } from "./../node_modules/vue";
+// import App from "./App.vue";
 import {ModelViewer} from "./components/modelViewer/ModelViewer";
 
 
@@ -13,6 +13,6 @@ async function run() {
 }
 
 
-createApp(App).mount('#App');
+// createApp(App).mount('#App');
 
 run();
