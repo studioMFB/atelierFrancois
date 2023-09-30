91c33e94b7be235851d16d4c7e424d033ecfdd78
studiomfb <57592762+studioMFB@users.noreply.github.com>
2023-09-30 12:54:35 +0100

    update config for Github

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
 const ASSET_URL = process.env.ASSET_URL || '';
+console.log("ASSET URL ", ASSET_URL);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,3 +19,9 @@ export default defineConfig({
     }
   }
 })
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/atelierFrancois/'
+    : '/'
+}
