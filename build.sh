950bdefe511851e0de6238e95e8dba7b5407fe70
Francois Brunet <francois.brunet@visr-vr.com>
2023-10-02 17:27:19 +0100

    adding dist subtree

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 # Build.
 npm run build
 
-git fetch
-git pull
+# git fetch
+# git pull
 # Commit changes to the master branch.
 git add --all 
 # git commit -a
@@ -11,7 +11,7 @@ git commit -m 'adding dist subtree'
 # Make GitHub pages a subtree of our master branch.
 #  git subtree push --prefix atelierFrancois https://github.com/studioMFB/atelierFrancois.git master --squash
  git subtree add --prefix atelierFrancois https://github.com/studioMFB/atelierFrancois.git master
- git subtree pull --prefix atelierFrancois https://github.com/studioMFB/atelierFrancois.git master
+#  git subtree pull --prefix atelierFrancois https://github.com/studioMFB/atelierFrancois.git master
  git subtree push --prefix atelierFrancois https://github.com/studioMFB/atelierFrancois.git master
 #  git subtree push --prefix dist origin atelierFrancois
 
