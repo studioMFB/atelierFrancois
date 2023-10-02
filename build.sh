17f0fbcb6d39505ef3415e28b00482f98f26d54f
Francois Brunet <francois.brunet@visr-vr.com>
2023-10-02 15:57:26 +0100

    update config

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 # Build.
 npm run build
 
+git fetch
+git pull
 # Commit changes to the master branch.
 git add --all 
 # git commit -a
 git commit -m 'adding dist subtree'
-git pull
 
 # Make GitHub pages a subtree of our master branch.
 #  git subtree push --prefix atelierFrancois https://github.com/studioMFB/atelierFrancois.git master --squash
