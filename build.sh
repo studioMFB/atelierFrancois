1d4223ec9f527e121c98d03b1d9fb9f6dc062f55
Francois Brunet <francois.brunet@visr-vr.com>
2023-10-02 12:05:59 +0100

    adding dist subtree

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ git commit -m 'adding dist subtree'
 
 # Make GitHub pages a subtree of our master branch.
 #  git subtree push --prefix atelierFrancois https://github.com/studioMFB/atelierFrancois.git master --squash
+ git subtree add --prefix atelierFrancois https://github.com/studioMFB/atelierFrancois.git master
  git subtree push --prefix atelierFrancois https://github.com/studioMFB/atelierFrancois.git master
 
 # Stop Terminal closing itself
