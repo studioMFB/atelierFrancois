5c3eeda35ab12eca07907373795bdf6ae8c7391e
Francois Brunet <francois.brunet@visr-vr.com>
2023-12-08 16:13:27 +0000

    Remove legacy code

--- src/App.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-// import ResourceExplorer from './components/ResourceExplorer.vue';
-</script>
-
-<!-- <template>
-    <ResourceExplorer></ResourceExplorer>
-</template> -->
