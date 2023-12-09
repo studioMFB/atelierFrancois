4be05b750a794a0de4f54f388aeee78cb395ee80
francois Brunet <studiomfb@gmail.com>
2023-12-09 21:43:57 +0000

    clean code

--- src/App.vue
+++ src/App.vue
@@ -15,24 +15,16 @@ import { FXAAShader } from "three/examples/jsm/shaders/FXAAShader.js";
 
 
 const rendererRef = ref() as Ref<RendererPublicInterface>;
-// const rendererRef = ref() as Ref<THREE.WebGLRenderer>;
 const sceneRef = ref() as Ref<THREE.Scene>;
 const cameraRef = ref() as Ref<THREE.PerspectiveCamera>;
-const furnitureRef = ref() as Ref<THREE.PerspectiveCamera>;
+const furnitureRef = ref() as Ref<any>;
 
-let localCamera: THREE.PerspectiveCamera;
 let raycaster: THREE.Raycaster;
 let pointer: THREE.Vector2;
-// let canvas: HTMLCanvasElement;
 
 let intersected: any;
 let INTERSECTED: any;
 const selectedObjects: any[] = [];
-
-// let composer: EffectComposer;
-// let renderPass: RenderPass;
-// let outline: OutlinePass;
-// let fxaaShader: ShaderPass;
 
 onMounted(() => {
   const renderer = rendererRef.value;
@@ -44,41 +36,22 @@ onMounted(() => {
   const scene = sceneRef.value;
   scene.children = [];
 
-  // canvas = document.getElementsByTagName("canvas")[0] as HTMLCanvasElement;
-  // console.log("CANVAS ", canvas);
-
-  localCamera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 1, 10000);
-  localCamera.position.set(-2, 3, 2);
-  cameraRef.value = localCamera;
+  cameraRef.value = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 1, 10000);
+  // cameraRef.value = new THREE.OrthographicCamera(-window.innerWidth/2, window.innerWidth/2, window.innerHeight/2, -window.innerHeight, 0.1, 1000);
 
   raycaster = new THREE.Raycaster();
   pointer = new THREE.Vector2();
-
-  // composer = new EffectComposer(renderer.renderer);
-  // renderPass = new RenderPass(scene, localCamera);
-  // composer.addPass(renderPass);
-  // outline = new OutlinePass(new THREE.Vector2(window.innerWidth, window.innerHeight), scene, localCamera);
-  // outline.edgeThickness = 2.0;
-  // outline.edgeStrength = 3.0;
-  // outline.visibleEdgeColor.set(0xffffff);
-  // composer.addPass(outline);
-
-  // fxaaShader = new ShaderPass(FXAAShader);
-  // fxaaShader.uniforms["resolution"].value.set(1 / window.innerWidth, 1 / window.innerHeight);
-  // composer.addPass(fxaaShader);
 
   scene.fog = new THREE.Fog(0xa0a0a0, 200, 1000);
 
   const GRID_SIZE = 5;
   const GRID_DIVISION = 10;
   const grid = new THREE.GridHelper(GRID_SIZE, GRID_DIVISION, "#888888", "#888888");
+
   scene.add(grid);
 
-  // canvas.addEventListener('click', onPointerDown, false);
-  // window.addEventListener("resize", windowResize);
-  // renderer.renderer.domElement.style.touchAction = "none";
-  // renderer.renderer.domElement.addEventListener("mousemove", mouseMove);
-  document.addEventListener("pointermove", mouseMove);
+  document.addEventListener("pointermove", (e: PointerEvent) => { onPointerMove(e) });
+// document.addEventListener("pointerdown", (e: PointerEvent) => { onPointerDown(e) });
 
   rendererRef.value = renderer;
   sceneRef.value = scene;
@@ -96,29 +69,17 @@ const onReady = (gltf: any) => {
 
   gltf.scene.traverse((child: THREE.Mesh) => {
     if (child.isMesh) {
-      if ( child.material ) child.material = (child.material as THREE.MeshStandardMaterial).clone();
-
       if (child.name.toLowerCase().includes("outline")) {
         child.material = matColor;
-        // sceneRef.value.children.push(child);
       }
       else {
         child.material = matToon;
         child.castShadow = true;
-        // sceneRef.value.children.push(child);
       }
     }
-    // sceneRef.value.children.push(child);
   });
 
   sceneRef.value.children.push(gltf.scene);
-}
-
-function addSelectedObjects(object: any) {
-  if (selectedObjects.length > 0) {
-    selectedObjects.pop();
-  }
-  selectedObjects.push(object);
 }
 
 function intersection() {
@@ -129,9 +90,6 @@ function intersection() {
     throw new Error(e.toString());
   }
 
-  // const vector = new THREE.Vector3(pointer.x, pointer.y, 1);
-  // const newVec = vector.unproject(cameraRef.value);
-  // raycaster = new THREE.Raycaster(cameraRef.value.position, newVec.sub(cameraRef.value.position).normalize());
   const intersects = raycaster.intersectObjects(sceneRef.value.children, true);
 
   if (intersects.length > 0) {
@@ -139,11 +97,9 @@ function intersection() {
 
     if (intersected != intersects[0].object && intersects[0].object.type === "Mesh") {
       intersected = intersects[0].object;
-      if (!intersected.name.toLowerCase().includes("outline")) {
+      if (!intersected.name.toLowerCase().includes("outline"))
         (intersected.material as THREE.MeshToonMaterial).color.set(0xf47653);
-      }
-      // addSelectedObjects(intersected);
-      // outline.selectedObjects = selectedObjects;
+
       console.log(intersected.name);
     }
   } else {
@@ -154,77 +110,25 @@ function intersection() {
   }
 }
 
-function mouseMove(event: PointerEvent) {
+function onPointerMove(event: PointerEvent) {
+  event.preventDefault();
+
   console.log("mouseMove");
 
-  // event.preventDefault();
   pointer.x = (event.offsetX / window.innerWidth) * 2 - 1;
   pointer.y = -(event.offsetY / window.innerHeight) * 2 + 1;
 
   intersection();
-  // composer.render();
 }
 
-// function windowResize() {
-//   cameraRef.value.aspect = window.innerWidth / window.innerHeight;
-//   cameraRef.value.updateProjectionMatrix();
-
-//   rendererRef.value.renderer.setSize(window.innerWidth, window.innerHeight);
-
-//   // composer.setSize(window.innerWidth, window.innerHeight);
-//   // fxaaShader.uniforms["resolution"].value.set(1 / window.innerWidth, 1 / window.innerHeight);
-// }
-
-
-// function onPointerMove(event: any) {
-//   event.preventDefault();
-//   // pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
-//   // pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
-//   // pointer.x = ( (event.clientX + canvas.offsetLeft) / canvas.width ) * 2 - 1;
-//   // pointer.y = - ( (event.clientY - canvas.offsetTop) / canvas.height ) * 2 + 1;
-// }
-
 // async function onPointerDown(event: any) {
-//   // pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
-//   // pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
-//   pointer.x = ((event.clientX + canvas.offsetLeft) / canvas.width) * 2 - 1;
-//   pointer.y = - ((event.clientY - canvas.offsetTop) / canvas.height) * 2 + 1;
-
-//   try {
-//     raycaster.setFromCamera(pointer, cameraRef.value);
-//   }
-//   catch (e: any) {
-//     throw new Error(e.toString());
-//   }
-
-//   const intersects = raycaster.intersectObjects(sceneRef.value.children, true);
-
-//   if (intersects.length > 0) {
-//     console.log("intersects ", intersects);
-
-//     if (!intersects[0].object)
-//       return;
-
-//     intersected = intersects[0].object as THREE.Mesh;
-//     if (!intersected.name.toLowerCase().includes("outline")) {
-//       (intersected.material as THREE.MeshToonMaterial).color.set(0xf47653);
-//     }
-//   }
-//   else {
-//     if (intersected && !intersected.name.toLowerCase().includes("outline"))
-//       (intersected.material as THREE.MeshToonMaterial).color.set(0xffffff);
-//   }
-
 // }
-
-// document.addEventListener("pointermove", (e: PointerEvent) => { onPointerMove(e) });
-// document.addEventListener("pointerdown", (e: PointerEvent) => { onPointerDown(e) });
-// canvas.addEventListener('click', onPointerDown, false);
 </script>
 
 <template>
   <Renderer ref="rendererRef" antialias :orbit-ctrl="{ enableDamping: true }" resize="window" shadow>
-    <Camera ref="cameraRef" :position="{ x: -2, y: 3, z: 2 }" />
+    <PerspectiveCamera ref="cameraRef" :position="{ x: -2, y: 3, z: 2 }" />
+    <!-- <OrthographicCamera ref="cameraRef" :position="{ x: -2, y: 3, z: 2 }" /> -->
     <Scene ref="sceneRef" background="#ded6d8">
 
       <HemisphereLight />
