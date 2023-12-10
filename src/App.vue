24a68e02f8fb31eac2c686940ce35ca5b3066aa1
francois Brunet <studiomfb@gmail.com>
2023-12-10 10:38:25 +0000

    wip gltf model on click

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { ref, onMounted, Ref } from 'vue';
 import * as THREE from "three";
 import {
   Renderer, Camera, RendererPublicInterface, Scene, PerspectiveCamera,
-  GltfModel, Plane, MeshPublicInterface,
+  GltfModel, Plane, MeshPublicInterface, Raycaster,
   LambertMaterial, ToonMaterial, PhongMaterial,
   DirectionalLight, HemisphereLight, AmbientLight, PointLight, SpotLight, StandardMaterial, ShadowMaterial, OrthographicCamera
 } from 'troisjs';
@@ -18,6 +18,7 @@ const rendererRef = ref() as Ref<RendererPublicInterface>;
 const sceneRef = ref() as Ref<THREE.Scene>;
 const cameraRef = ref() as Ref<THREE.PerspectiveCamera>;
 const furnitureRef = ref() as Ref<any>;
+const furnitureColourRef = ref() as Ref<string>
 
 let raycaster: THREE.Raycaster;
 let pointer: THREE.Vector2;
@@ -34,13 +35,13 @@ onMounted(() => {
   renderer.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
   const scene = sceneRef.value;
-  scene.children = [];
+  // scene.children = [];
 
   cameraRef.value = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 1, 10000);
   // cameraRef.value = new THREE.OrthographicCamera(-window.innerWidth/2, window.innerWidth/2, window.innerHeight/2, -window.innerHeight, 0.1, 1000);
 
-  raycaster = new THREE.Raycaster();
-  pointer = new THREE.Vector2();
+  // raycaster = new THREE.Raycaster();
+  // pointer = new THREE.Vector2();
 
   scene.fog = new THREE.Fog(0xa0a0a0, 200, 1000);
 
@@ -50,8 +51,10 @@ onMounted(() => {
 
   scene.add(grid);
 
-  document.addEventListener("pointermove", (e: PointerEvent) => { onPointerMove(e) });
-// document.addEventListener("pointerdown", (e: PointerEvent) => { onPointerDown(e) });
+  // document.addEventListener("pointermove", (e: PointerEvent) => { onPointerMove(e) });
+  // document.addEventListener("pointerdown", (e: PointerEvent) => { onPointerDown(e) });
+
+  furnitureColourRef.value = '#ffffff';
 
   rendererRef.value = renderer;
   sceneRef.value = scene;
@@ -78,81 +81,110 @@ const onReady = (gltf: any) => {
       }
     }
   });
-
-  sceneRef.value.children.push(gltf.scene);
+  
+  furnitureRef.value = gltf.scene;
+  // sceneRef.value.children.push(gltf.scene);
 }
 
-function intersection() {
-  try {
-    raycaster.setFromCamera(pointer, cameraRef.value);
-  }
-  catch (e: any) {
-    throw new Error(e.toString());
-  }
+// function intersection() {
+//   try {
+//     raycaster.setFromCamera(pointer, cameraRef.value);
+//   }
+//   catch (e: any) {
+//     throw new Error(e.toString());
+//     return;
+//   }
 
-  const intersects = raycaster.intersectObjects(sceneRef.value.children, true);
+//   const intersects = raycaster.intersectObjects(sceneRef.value.children, true);
 
-  if (intersects.length > 0) {
-    console.log(intersects);
+//   if (intersects.length > 0) {
+//     console.log(intersects);
 
-    if (intersected != intersects[0].object && intersects[0].object.type === "Mesh") {
-      intersected = intersects[0].object;
-      if (!intersected.name.toLowerCase().includes("outline"))
-        (intersected.material as THREE.MeshToonMaterial).color.set(0xf47653);
+//     if (intersected != intersects[0].object && intersects[0].object.type === "Mesh") {
+//       intersected = intersects[0].object;
+//       if (!intersected.name.toLowerCase().includes("outline"))
+//         (intersected.material as THREE.MeshToonMaterial).color.set(0xf47653);
 
-      console.log(intersected.name);
-    }
-  } else {
-    if (intersected && !intersected.name.toLowerCase().includes("outline"))
-      (intersected.material as THREE.MeshToonMaterial).color.set(0xffffff);
+//       console.log(intersected.name);
+//     }
+//   } else {
+//     if (intersected && !intersected.name.toLowerCase().includes("outline"))
+//       (intersected.material as THREE.MeshToonMaterial).color.set(0xffffff);
 
-    intersected = null;
-  }
-}
+//     intersected = null;
+//   }
+// }
 
-function onPointerMove(event: PointerEvent) {
-  event.preventDefault();
+// function onPointerMove(event: PointerEvent) {
+//   event.preventDefault();
 
-  console.log("mouseMove");
+//   console.log("mouseMove");
 
-  pointer.x = (event.offsetX / window.innerWidth) * 2 - 1;
-  pointer.y = -(event.offsetY / window.innerHeight) * 2 + 1;
+//   pointer.x = (event.offsetX / window.innerWidth) * 2 - 1;
+//   pointer.y = -(event.offsetY / window.innerHeight) * 2 + 1;
 
-  intersection();
-}
+//   intersection();
+// }
 
 // async function onPointerDown(event: any) {
 // }
+
+// function onPointerOver({over}:any) {
+//   console.log("onPointerOver");
+//   // furnitureRef.value.material.color.set(0xff0000);
+//   furnitureColourRef.value = over ? '#ff0000' : '#ffffff';
+// }
+
+function gltfOnClick(e:Event){
+  alert('Click');
+  console.log(e);
+}
+
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+  cameraRef.value.aspect = window.innerWidth / window.innerHeight
+  cameraRef.value.updateProjectionMatrix()
+  rendererRef.value.renderer.setSize(window.innerWidth, window.innerHeight)
+}
 </script>
 
 <template>
-  <Renderer ref="rendererRef" antialias :orbit-ctrl="{ enableDamping: true }" resize="window" shadow>
+  <Renderer ref="rendererRef" :pointer="{ intersectMode: 'frame' }" antialias :orbit-ctrl="{ enableDamping: true }"
+    resize="window" shadow>
+
     <PerspectiveCamera ref="cameraRef" :position="{ x: -2, y: 3, z: 2 }" />
     <!-- <OrthographicCamera ref="cameraRef" :position="{ x: -2, y: 3, z: 2 }" /> -->
+
     <Scene ref="sceneRef" background="#ded6d8">
 
       <HemisphereLight />
       <!-- <DirectionalLight :position="{ x: 55, y: 300, z: 70 }" :intensity=".07"
-      cast-shadow :shadow-camera="{ bias:0.005, radius:200 }"
+        cast-shadow :shadow-camera="{ bias:0.005, radius:200 }"
       :shadow-map-size="{ width: 6000, height: 6000 }" :angle="Math.PI/3" :penumbra="0.3"/> -->
 
       <SpotLight color="#ffffff" :position="{ x: 5, y: 9, z: 7 }" :intensity="0.01" :angle="Math.PI / 3" :penumbra="0.3"
         cast-shadow :shadow-map-size="{ width: 200, height: 200 }"
         :shadow-camera="{ near: 10, far: 200, bias: 0.005, radius: 20 }" />
 
-
       <PointLight color="#ff0040" :position="{ x: 0, y: 5, z: 2 }" :intensity=".08"></PointLight>
       <PointLight color="#0040ff" :position="{ x: 0, y: 4, z: 2 }" :intensity=".08"></PointLight>
       <PointLight color="#80ff80" :position="{ x: 2, y: 9, z: -2 }" :intensity=".08"></PointLight>
       <PointLight color="#ffaa00" :position="{ x: -2, y: 6, z: 2 }" :intensity=".08"></PointLight>
 
-      <GltfModel ref="furnitureRef" src="/assets/models/table/1/littlewood_furniture.gltf" @load="onReady" />
+      <!-- <Raycaster intersect-mode="frame" intersect-recursive @click="onPointerOver" /> -->
+      <GltfModel ref="furnitureRef" src="/assets/models/table/1/littlewood_furniture.gltf" 
+      @click="gltfOnClick" @load="onReady">
+      <!-- @pointerOver="onPointerOver" @click="gltfOnClick" @load="onReady"> -->
+      <!-- <LambertMaterial :color="furnitureColourRef" />   -->
+    </GltfModel>
       <!-- <GltfModel ref="furnitureRef" src="/assets/models/van/1/van.glb" @load="onReady" /> -->
 
       <Plane :width="2000" :height="2000" :rotation="{ x: -Math.PI / 2 }" receive-shadow>
         <ShadowMaterial receive-shadow color="#888888" :transparent=true :opacity=0.005 :side=THREE.DoubleSide>
         </ShadowMaterial>
       </Plane>
+
+      <!-- <ArrowHelper></ArrowHelper> -->
 
     </Scene>
   </Renderer>
