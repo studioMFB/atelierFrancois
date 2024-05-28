6d1ac18cab4ab7b51b0b325453cec542259c02e2
Francois Brunet <francois.brunet@visr-vr.com>
2024-05-28 14:55:52 +0100

    Make text non selectable in headerBar and side menu.

--- frontend/src/views/HeaderBar.vue
+++ frontend/src/views/HeaderBar.vue
@@ -20,19 +20,11 @@ import SideMenu from '@/components/menus/SideMenu.vue';
 </template>
 
 <style scoped lang="scss">
-// .menu-wrapper {
-//     // display: flex;
-//     // flex: 2;
-//     // justify-content: flex-start;
-//     width: 100%;
-// }
-
 .header-wrapper {
+  user-select: none;
   z-index: 10;
   position: absolute;
   display: flex;
-  // align-items: center;
-  // justify-content: center;
   justify-content: space-between;
   flex-grow: 1;
   top: 0;
@@ -40,7 +32,6 @@ import SideMenu from '@/components/menus/SideMenu.vue';
   right: 0;
   height: var(--header-height);
   padding: 0 var(--spacing-3);
-  // margin: 0 auto;
   border-bottom: 1px solid var(--color-2);
   background-color: var(--header-background);
 }
@@ -62,40 +53,19 @@ import SideMenu from '@/components/menus/SideMenu.vue';
 .header-wrapper-inner-wrap {
   display: flex;
   align-items: center;
-  // flex-direction: row;
-  // justify-content: space-between;
-  // justify-content: center;
-
-  // margin: 0 auto;
   width: 100%;
   height: 100%;
-  // max-width: var(--max-width);
 }
 
 .logo {
-  // display: flex;
-  // align-items: center;
-  // justify-content: center;
-  // flex-grow: 1;
-  // flex-shrink: 2;
   width: 100%;
 }
 
 .logo-link {
-  // width: auto;
-  // height: 2rem;
-  // padding: .5rem 1rem;
-  // margin: 0 .7rem;
   font-weight: 800;
   font-size: 25px;
   color: var(--color-text);
   text-decoration: none;
   transition: all .1s ease-in-out;
-
-  // margin: 0;
-  //   max-height: 58px;
-  //   max-width: 250px;
-  //   height: auto;
-  //   width: auto;
 }
 </style>
