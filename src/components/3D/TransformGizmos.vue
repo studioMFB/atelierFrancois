6960cb3cea44607f873a8188e75b5049ecd7c84f
francois Brunet <studiomfb@gmail.com>
2025-01-08 16:37:07 +0000

    tidy

--- src/components/3D/TransformGizmos.vue
+++ src/components/3D/TransformGizmos.vue
@@ -1,90 +1,11 @@
-<!-- <script setup lang="ts">
-import { computed, inject, provide, type Ref } from 'vue';
-
-import { Box3, Line, Mesh, Object3D, PerspectiveCamera, Scene, Vector3 } from 'three';
-import { TransformControls, TransformControlsGizmo } from 'three/examples/jsm/controls/TransformControls.js';
-
-
-const props = defineProps<{
-    canvas?: HTMLCanvasElement
-}>();
-
-const canvas = computed(() => props.canvas) as Ref<HTMLCanvasElement>;
-const scene = inject("MainScene") as Scene | undefined;
-const camera = inject("PerspectiveCamera") as PerspectiveCamera;
-
-if (!canvas.value) {
-    throw new Error("Canvas is undefined");
-}
-if (!scene) {
-    throw new Error("MainScene is not provided");
-}
-if (!camera) {
-    throw new Error("PerspectiveCamera is not provided");
-}
-
-const transformControls = new TransformControls(camera, canvas.value);
-transformControls.setMode('translate');
-
-scene.add(transformControls);
-
-transformControls.position.set(0,0,0); // Set the position of TransformControls to the center
-
-// Ensure TransformControls has children before calculating its bounding box
-if (transformControls.children.length > 0) {
-    const modelBoundingBox = new Box3().setFromObject(transformControls); // Calculate the bounding box
-    const center = modelBoundingBox.getCenter(new Vector3()); // Get the center of the bounding box
-    transformControls.position.set(center.x, center.y, center.z); // Set the position of TransformControls to the center
-}
-
-// Main gizmo, arrows and squares
-(transformControls.children[0] as TransformControlsGizmo).gizmo.translate.traverse((child: Object3D) => {
-    const _child = child as Mesh;
-
-    if (_child.isMesh) {
-        if (child.name === 'XYZ') {
-            (_child.material as THREE.MeshBasicMaterial).color.set(0x0e73e6); // Set color for XYZ axis
-            (_child.material as THREE.MeshBasicMaterial).opacity = 0.5; // Set opacity
-        } else {
-            (_child.material as THREE.MeshBasicMaterial).visible = false; // Hide other meshes
-        }
-    }
-});
-
-// Pickers
-// Not sure it does much.
-(transformControls.children[0] as TransformControlsGizmo).picker.translate.traverse((child: Object3D) => {
-    const _child = child as Mesh;
-
-    if (_child.isMesh) {
-        (_child.material as THREE.MeshBasicMaterial).visible = false; // Hide picker meshes
-    }
-});
-
-// Helper transform lines axis
-(transformControls.children[0] as TransformControlsGizmo).helper.translate.traverse((child: Object3D) => {
-    if ((child as Line).isLine) {
-        ((child as Mesh).material as THREE.MeshBasicMaterial).visible = false; // Hide helper lines
-    }
-});
-
-// Provide the TransformControls instance to the parent context
-if (transformControls) {
-    provide("TransformGizmos", transformControls);
-} else {
-    console.warn("Transform controls are not initialized correctly.");
-}
-</script>
-
-<template>
-    <slot></slot>
-</template> -->
-
 <script setup lang="ts">
 import { computed, inject, onMounted, provide, type Ref } from 'vue';
-import { Box3, BufferGeometry, Float32BufferAttribute, Line, LineBasicMaterial, Mesh, Object3D, PerspectiveCamera, Scene, Vector3 } from 'three';
+
+import { BufferGeometry, Float32BufferAttribute, Line, LineBasicMaterial, Mesh, Object3D, PerspectiveCamera, Scene } from 'three';
 import { TransformControls, TransformControlsGizmo } from 'three/examples/jsm/controls/TransformControls.js';
+
 import { COLOURS } from './constants';
+
 
 const props = defineProps<{
     canvas?: HTMLCanvasElement
@@ -100,13 +21,11 @@ if (!scene || !camera) {
 
 let transformControls: TransformControls | null = null;
 
-// onMounted(() => {
 if (!canvas.value) {
     console.error("Canvas is undefined");
-    // return;
 }
 
-// Initialize TransformControls
+// Initialise TransformControls
 transformControls = new TransformControls(camera, canvas.value);
 transformControls.setMode('translate');
 scene.add(transformControls);
@@ -114,15 +33,15 @@ scene.add(transformControls);
 // Provide TransformControls to the parent context
 provide("TransformGizmos", transformControls);
 
-// Customize Gizmo appearance
+// Customise Gizmo appearance
 const gizmo = transformControls.children.find((child) => child.type === 'TransformControlsGizmo');
 if (gizmo) {
 
-    const createLine = (axis: 'x' | 'z') => {
+    const createAxis = (axis: 'x' | 'z') => {
         const lineGeometry = new BufferGeometry();
         const vertices = axis === 'x'
-            ? [0, 0, 0, .01, 0, 0] // X-axis: Line from (0, 0, 0) to (1, 0, 0)
-            : [0, 0, 0, 0, 0, .01]; // Z-axis: Line from (0, 0, 0) to (0, 0, 1)
+            ? [0, 0, 0, .01, 0, 0] // X-axis: Line from (0, 0, 0) to (.01, 0, 0)
+            : [0, 0, 0, 0, 0, .01]; // Z-axis: Line from (0, 0, 0) to (0, 0, .01)
         lineGeometry.setAttribute('position', new Float32BufferAttribute(vertices, 3));
         const lineMaterial = new LineBasicMaterial({
             color: COLOURS.GIZMOS,
@@ -135,29 +54,28 @@ if (gizmo) {
         if ((child as Mesh).isMesh) {
             const mesh = child as Mesh;
 
-            (mesh.material as THREE.MeshBasicMaterial).color.set(COLOURS.GIZMOS); // XYZ axis color
-            (mesh.material as THREE.MeshBasicMaterial).opacity = 0.5;      // Transparency
+            (mesh.material as THREE.MeshBasicMaterial).color.set(COLOURS.GIZMOS); // colour
+            (mesh.material as THREE.MeshBasicMaterial).opacity = 0.5; // Transparency
 
             if (mesh.name === 'XYZ') {
                 // Customise the centre of the Gizmos (cube) appearance
                 (mesh.material as THREE.MeshBasicMaterial).visible = true;
-                // mesh.position.addVectors(mesh.position, new Vector3(0,1,0));
             }
             else if (mesh.name === 'X') {
                 // Remove the existing arrow
                 (gizmo as TransformControlsGizmo).gizmo.translate.remove(mesh);
                 // Replace with:
-                const xLine = createLine('x');
-                xLine.name = 'X'; // Preserve the name for identification
-                (gizmo as TransformControlsGizmo).gizmo.translate.add(xLine);
+                const xAxis = createAxis('x');
+                xAxis.name = 'X'; // Preserve the name for identification
+                (gizmo as TransformControlsGizmo).gizmo.translate.add(xAxis);
             }
             else if (mesh.name === 'Z') {
                 // Remove the existing arrow
                 (gizmo as TransformControlsGizmo).gizmo.translate.remove(mesh);
                 // Replace with:
-                const zLine = createLine('z');
-                zLine.name = 'Z'; // Preserve the name for identification
-                (gizmo as TransformControlsGizmo).gizmo.translate.add(zLine);
+                const zAxis = createAxis('z');
+                zAxis.name = 'Z'; // Preserve the name for identification
+                (gizmo as TransformControlsGizmo).gizmo.translate.add(zAxis);
             }
             else {
                 (mesh.material as THREE.MeshBasicMaterial).visible = false;   // Hide other meshes
@@ -169,19 +87,7 @@ if (gizmo) {
     (gizmo as TransformControlsGizmo).helper.translate.traverse((child: Object3D) => {
         child.layers.set(1); // Don't highlight objects above layer 0
     });
-
-    // (gizmo as TransformControlsGizmo).picker.translate.traverse((child: Object3D) => {
-    //     if ((child as Mesh).isMesh) {
-    //         const mesh = child as Mesh;
-    //         (mesh.material as THREE.MeshBasicMaterial).color.set(COLOURS.PREVIEW);
-    //         // (mesh.material as THREE.MeshBasicMaterial).visible = false;   // Hide other meshes
-    //     }
-
-    //     child.visible = false;
-    // });
-
 }
-// });
 </script>
 
 <template>
